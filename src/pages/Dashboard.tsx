604b1f40879f4978397ce53fa23849fa373d66df
jayant-ssharma <jayant0078an@gmail.com>
2026-08-04 00:20:09 +0530

    feat: add group creation with Supabase RLS

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,57 @@
+import { useEffect, useState } from "react";
+import { getGroups } from "../services/group";
+import { createGroup } from "../services/group";
+import { useAuth } from "../components/context/AuthContext";
+
+type Group = {
+  id: number;
+  name: string;
+  created_at?: string;
+  created_by?: string | null;
+};
+
 const Dashboard = () => {
+  const [groups, setGroups] = useState<Group[]>([]);
+
+  useEffect(() => {
+    loadGroups();
+  }, []);
+
+  async function loadGroups() {
+    const { data, error } = await getGroups();
+
+    if (error) {
+      console.log(error);
+    } else {
+      setGroups((data as Group[]) ?? []);
+    }
+  }
+ const { user } = useAuth();
+ const handleCreate = async () => {
+  if (!user) return;
+
+  console.log("User ID:", user.id);
+console.log("User:", user);
+  const { error } = await createGroup(
+    "Trip Goa",
+    user.id
+  );
+
+  if (error) {
+    console.log(error.message);
+    return;
+  }
+
+  loadGroups();
+};
+console.log(groups)
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+       <button onClick={handleCreate}>
+Create Group
+</button>
+    </div>
+  );
+};
 
 export default Dashboard
---
+++ src/services/group.ts
@@ -0,0 +1,54 @@
+import { supabase } from "../lib/supabase";
+export async function getGroups() {
+  const { data, error } = await supabase!
+    .from("groups")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  return { data, error };
+}
+
+export async function createGroup(
+  name: string,
+  createdBy: string
+) {
+    console.log("createdBy:", createdBy);
+  const { data, error } = await supabase!
+    .from("groups")
+    
+    .insert({
+      name,
+      created_by: createdBy,
+    })
+    
+    .select();
+    console.log(data);
+console.log(error);
+
+  return { data, error };
+}
+
+export async function deleteGroup(id: number) {
+  const { error } = await supabase!
+    .from("groups")
+    .delete()
+    .eq("id", id);
+
+  return { error };
+}
+
+export async function updateGroup(
+  id: number,
+  name: string
+) {
+  const { data, error } = await supabase!
+    .from("groups")
+    .update({
+      name,
+    })
+    .eq("id", id)
+    .select();
+
+  return { data, error };
+}
+
